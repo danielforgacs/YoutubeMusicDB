e2a39eb7f24ee139dadee2f861813b5f7f47d05e
df@ <@>
2020-08-01 00:43:57 +0100

    started app python modules.

---
+++ app/main.py
@@ -0,0 +1,12 @@
+import os
+import psycopg2
+import app.config as config
+
+
+conn = psycopg2.connect(
+    host=os.getenv('DB_HOST'),
+    port=os.getenv('DB_PORT'),
+    dbname=os.getenv('DB_DBNAME'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+)
