8f425c2c0c4a1a1df6881e9aa19b2643393ef71a
df@ <@>
2020-08-03 01:27:35 +0100

    app now accepts POST application/json

--- app/main.py
+++ app/main.py
@@ -16,6 +16,34 @@ app = flask.Flask(__name__)
 
 
 
+@app.route('/', methods=['POST'])
+def post_playlist():
+    data = flask.request
+    print()
+    # print(data)
+    # print(type(data))
+    # print('data.args: ', data.args)
+    # print('data.data: ', data.data)
+    # print('data.form: ', data.form)
+    # print('data.from_values(): ', data.from_values())
+    # print('data.get_data(): ', data.get_data())
+    # print('data.get_json(): ', data.get_json())
+    # print('data.json: ', data.json)
+    # print('data.values: ', data.values)
+    print()
+    # print(data.get_json(force=True))
+    # print(data.get_json())
+    print(flask.request.json)
+    print(flask.request.json['a'])
+    print(type(flask.request.json['a']))
+    # print(flask.request.get_json())
+    # print(dir(data.form))
+    # print(data.form.to_dict())
+    print()
+    return 'askdfhj\n'
+
+
+
 if __name__ == '__main__':
     app.run(
         debug=os.getenv('DEBUG'),
