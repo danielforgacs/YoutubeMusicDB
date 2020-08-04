714ae9ad699aeee8189511c91c91b41ff8d5edcd
df@ <@>
2020-08-04 19:07:47 +0100

    wrote context class to pg connect

--- app/data.py
+++ app/data.py
@@ -2,20 +2,37 @@ import os
 import psycopg2
 
 
-conn = psycopg2.connect(
-    host=os.getenv('DB_HOST'),
-    port=os.getenv('DB_PORT'),
-    dbname=os.getenv('DB_DBNAME'),
-    user=os.getenv('DB_USER'),
-    password=os.getenv('DB_PASSWORD'),
-)
+
+class PGConnection:
+    def __init__(self, dbname=None):
+        self.dbname = dbname or os.getenv('DB_DBNAME')
+
+    def __enter__(self, *args, **kwargs):
+        self.conn = psycopg2.connect(
+            host=os.getenv('DB_HOST'),
+            port=os.getenv('DB_PORT'),
+            dbname=self.dbname,
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+        )
+        return self.conn
+
+
+    def __exit__(self, errtype, err, traceback):
+        self.conn.close()
+
 
 
 def create_playlist(title, ytid):
-    sql = "INSERT INTO playlist (title, youtubeid) VALUES (%s, %s)"
-    cur = conn.cursor()
-    cur.execute(sql, (title, ytid))
-    conn.commit()
+    sql = """
+INSERT INTO playlist (title, youtubeid)
+VALUES (%s, %s)
+"""
+
+    with PGConnection() as conn:
+        cur = conn.cursor()
+        cur.execute(sql, (title, ytid))
+        conn.commit()
 
 
 
