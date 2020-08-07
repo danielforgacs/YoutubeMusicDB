62c6cb7be544617d78cc600195d0b5b48e394581
df@ <@>
2020-08-07 12:24:43 +0100

    wip

--- app/youtube.py
+++ app/youtube.py
@@ -9,49 +9,14 @@ urls = [
 ]
 
 
-ytdl_opts = {
-    'autonumber': None,
-    'autonumber_start': 1,
-    'consoletitle': False,
-    'continuedl': True,
-    'dump_single_json': False,
-    'encoding': None,
-    'extract_flat': False,
-    'forceid': False,
-    'forcetitle': False,
-    'forceurl': False,
-    'ignoreerrors': False,
-    'keepvideo': False,
-    'listformats': None,
-    'matchtitle': None,
-    'max_downloads': None,
-    'nopart': False,
-    'noplaylist': False,
-    'noprogress': False,
-    'outtmpl': '%(title)s-%(id)s.%(ext)s',
-    'playlist_items': None,
-    'playlistend': None,
-    'playlistrandom': None,
-    'playlistreverse': None,
-    'playliststart': 1,
-    'quiet': True,
-    'rejecttitle': None,
-    'restrictfilenames': False,
-    'simulate': True,
-    'skip_download': False,
-    'test': False,
-    'updatetime': True,
-    'usetitle': None,
-    'writeannotations': False,
-    'writedescription': False,
-    'writeinfojson': False,
-    'youtube_include_dash_manifest': True,
-}
-
 
 class Youtube(youtube_dl.YoutubeDL):
-    def __init__(self, url=None):
-        super().__init__(params=ytdl_opts, auto_init=True)
+    def __init__(self, url=None, params=dict(), do_download=False):
+        params.update({'quiet': True})
+
+        if not do_download:
+            params.update({'skip_download': True})
+        super().__init__(params=params, auto_init=True)
         self.url = url
 
     def fetch_info(self):
