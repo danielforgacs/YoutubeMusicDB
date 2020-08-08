b2b65d75d83d022926a78617e0d47d21f3277cea
df@ <@>
2020-08-08 04:39:20 +0100

    refactored, cleaned tests

--- tests/main_test.py
+++ tests/main_test.py
@@ -6,7 +6,12 @@ import app.youtube as ytdl
 
 youtubeids = [
     'PL9YsudagsL6hicXrha4zBId875lRXxc32',
+    'https://www.youtube.com/playlist?list=PL9YsudagsL6hicXrha4zBId875lRXxc32',
+    'https://www.youtube.com/watch?v=HJq-6y2IYEQ',
+    'HJq-6y2IYEQ',
+    'FIQ2F3T1ydM',
 ]
+
 
 
 
@@ -20,48 +25,19 @@ def client():
 
 @pytest.mark.parametrize('ytid', youtubeids)
 def test_post_playlist(client, ytid):
-    expected = {
-        'id': 'PL9YsudagsL6hicXrha4zBId875lRXxc32',
-        'title': 'AAAA',
-        'uploader_id': 'forgacsdaniel',
-        'videos': [
-        {
-            'alt_title': None,
-            'duration': 281,
-            'id': 'HJq-6y2IYEQ',
-            'title': 'Drop D Funk - Dr Funk Original - Blast Inspired - Street Sessions Newquay',
-            'upload_date': '20191105',
-            'uploader': 'Dr Funk',
-            'uploader_id': 'UCW8tq_mhu8pVDH2UIGRwuSg'
-        },
-        {
-            'alt_title': None,
-            'duration': 153,
-            'id': 'FIQ2F3T1ydM',
-            'title': 'los pollos hermanos commercial HD',
-            'upload_date': '20111008',
-            'uploader': 'ibsuser1',
-            'uploader_id': 'ibsuser1'
-        }
-        ]
-    }
-    response = client.post('/', json={'id': ytid})
-    data = response.get_json()
-
-    assert data == expected
-
-
-
-@pytest.mark.parametrize('ytid', youtubeids)
-def test_post_playlist_02(client, ytid):
     youtube = ytdl.Youtube()
     youtube.url = ytid
     youtube.fetch_info()
 
+    if youtube.playlist:
+        expected = youtube.playlist.as_dict
+    else:
+        expected = youtube.video.as_dict
+
     response = client.post('/', json={'id': ytid})
     data = response.get_json()
 
-    assert youtube.playlist.as_dict == data
+    assert data == expected
 
 
 
