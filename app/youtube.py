d9c671170951a57655d0960bd8979dd4f7454363
df@ <@>
2020-08-11 21:56:53 +0100

    fixeing youtube classes

--- app/youtube.py
+++ app/youtube.py
@@ -11,10 +11,12 @@ class BaseEntity:
             if attr in self._attrs:
                 setattr(self, attr, attrs[attr])
 
+        self.youtube = attrs['youtube']
+
 
 
     def print_info(self):
-        for attr in self._attrs:
+        for attr in ['pk']+self._attrs:
             print('{:<50}{}'.format(attr, getattr(self, attr)))
 
 
@@ -77,6 +79,16 @@ class Video(BaseEntity):
         # 'extractor_key'
     ]
 
+    def __init__(self, attrs):
+        self.playlist = None
+        super().__init__(attrs=attrs)
+        self.pk = datab.insert_video(vdata=self.as_dict)
+
+
+    def print_info(self):
+        super().print_info()
+        print('{:<50}{}'.format('playlist', self.playlist))
+
 
 
 
@@ -97,25 +109,25 @@ class Playlist(BaseEntity):
 
     def __init__(self, attrs):
         super().__init__(attrs=attrs)
+        self.pk = datab.insert_playlist(pldict=self.as_dict)
         self._videos = []
-        self.dbid = None
+        # self.dbid = None
+
 
 
     @property
     def videos(self):
-        if not self.dbid:
-            return
-
         for item in self.entries:
-            ytdl = Youtube(
+            yout = Youtube(
                 url=item['url'],
                 params=self.youtube.params,
                 do_download=self.youtube.do_download
             )
-            ytdl.fetch_info()
-            video = ytdl.video
-            video.pldbid = self.dbid
-            self._videos.append(video)
+            # ytdl.fetch_info()
+            # video = ytdl.video
+            # video.pldbid = self.dbid
+            yout.video.playlist = self
+            self._videos.append(yout.video)
 
         return self._videos
 
@@ -123,7 +135,8 @@ class Playlist(BaseEntity):
     @property
     def as_dict(self):
         data = super().as_dict
-        data['videos'] = [video.as_dict for video in self.videos]
+        # data['videos'] = [video.as_dict for video in self.videos]
+        data['videos'] = [{}]
         del data['entries']
         return data
 
@@ -133,7 +146,7 @@ class Playlist(BaseEntity):
 
 
 class Youtube(youtube_dl.YoutubeDL):
-    def __init__(self, url=None, params=None, do_download=False):
+    def __init__(self, url, params=None, do_download=False):
         self.params = params
         self.url = url
         self.do_download = do_download
@@ -147,6 +160,8 @@ class Youtube(youtube_dl.YoutubeDL):
 
         super().__init__(params=params, auto_init=True)
 
+        self.fetch_info()
+
 
 
     def fetch_info(self):
@@ -156,19 +171,19 @@ class Youtube(youtube_dl.YoutubeDL):
                 download=self.do_download,
                 process=self.do_download,
             )
+            result['youtube'] = self
 
             if '_type' in result.keys():
                 self.playlist = Playlist(attrs=result)
-                self.playlist.youtube = self
-                print('LHGKJHG')
-                self.playlist.dbid = datab.insert_playlist(playlist=self.playlist)
+                # self.playlist.youtube = self
+                # playlist.pk = datab.insert_playlist(playlist=self.playlist)
                 # self.playlist.videos
 
 
             else:
                 self.video = Video(attrs=result)
-                self.video.youtube = self
-                datab.insert_video(video=self.video)
+                # self.video.youtube = self
+                # datab.insert_video(video=self.video)
 
 
 
@@ -181,15 +196,28 @@ if __name__ == '__main__':
         # 'PL9YsudagsL6hicXrha4zBId875lRXxc32',
         # 'https://www.youtube.com/watch?v=HJq-6y2IYEQ',
         # 'HJq-6y2IYEQ',
-        # 'FIQ2F3T1ydM',
+        'FIQ2F3T1ydM',
     ]
 
     for url in urls:
         print('\n', '-'*79)
 
-        ytdl = Youtube()
-        ytdl.url = url
-        ytdl.fetch_info()
+        yout = Youtube(url=url)
+        print(yout.playlist)
+
+        if yout.playlist:
+            yout.playlist.print_info()
+
+            for vid in yout.playlist.videos:
+                print('\t', vid)
+                vid.print_info()
+
+        else:
+            yout.video.print_info()
+        # yout.fetch_info()
+        # yout.video.print_info()
+        # ytdl.url = url
+        # ytdl.fetch_info()
 
         # if ytdl.playlist:
         #     ytdl.playlist.print_info()
