24cf9de34b51479773275789930b33cade522fe1
df@ <@>
2020-08-14 17:36:05 +0100

    addeddddd example curl requests

---
+++ tests/curl_requests.sh
@@ -0,0 +1,9 @@
+# curl http://127.0.0.1:5000 -H 'Content-type: application/json' -d '{"id": "FIQ2F3T1ydM"}'
+# curl http://127.0.0.1:5000 -H 'Content-type: application/json' -d '{"id": "PL9YsudagsL6hicXrha4zBId875lRXxc32"}'
+# curl http://127.0.0.1:5000 -H 'Content-type: application/json' -d '{"id": "FIQ2F3T1ydM"}'
+# curl http://127.0.0.1:5000 -H 'Content-type: application/json' -d '{"id": "PL9YsudagsL6hicXrha4zBId875lRXxc32"}'
+
+
+
+# curl http://127.0.0.1:5000 -H 'Content-type: application/json' -d '{"id": "PL9YsudagsL6hicXrha4zBId875lRXxc32"}'
+curl http://127.0.0.1:5000/download -H 'Content-type: application/json' -d '{"id": "PL9YsudagsL6hicXrha4zBId875lRXxc32"}'
