ddd2dfccf863aad3265bb8625a5091a337d620fa
df@ <@>
2020-08-16 14:01:12 +0100

    refactor

--- tests/curl_requests.sh
+++ tests/curl_requests.sh
@@ -1,17 +1,12 @@
 # playlist: AAAA
 # https://www.youtube.com/playlist?list=PL9YsudagsL6hicXrha4zBId875lRXxc32
 
-headerjson="Content-type: application/json"
-post_playlist_url=http://127.0.0.1:5000
-download_playlist_url=http://127.0.0.1:5000/download
-playlist1=PL9YsudagsL6hicXrha4zBId875lRXxc32
-
 
 echo "--> post playlist:"
-curl $post_playlist_url -H "$headerjson" -d "{\"id\": \"$playlist1\"}"
+curl http://127.0.0.1:5000 -H "Content-type: application/json" -d "{\"id\": \"PL9YsudagsL6hicXrha4zBId875lRXxc32\"}"
 
 echo "--> download playlist:"
-curl $download_playlist_url -H "$headerjson" -d "{\"id\": \"$playlist1\"}"
+curl http://127.0.0.1:5000/download -H "Content-type: application/json" -d "{\"id\": \"PL9YsudagsL6hicXrha4zBId875lRXxc32\"}"
 
 echo "--> downloading archive:"
 curl http://localhost:5000/archive -o download.zip
