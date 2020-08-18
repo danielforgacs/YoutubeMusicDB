b976a7cd4e6a80f02b40f759bdd133f6dfb9569f
df@ <@>
2020-08-18 20:02:08 +0100

    started tests setup module

---
+++ tests/setup.py
@@ -0,0 +1,15 @@
+YOUTUBE_PLAYLISTS = [
+    'https://www.youtube.com/playlist?list=PL9YsudagsL6hicXrha4zBId875lRXxc32',
+    'https://www.youtube.com/playlist?list=PL9YsudagsL6h0n4ew9ibbicfGFIPdUKMU',
+    'https://www.youtube.com/playlist?list=PL9YsudagsL6ipb5Yd7QKz0x9byLncwEs_',
+]
+YOUTUBE_VIDEOS = [
+    'https://www.youtube.com/watch?v=HJq-6y2IYEQ',
+    'https://www.youtube.com/watch?v=FIQ2F3T1ydM',
+    'https://www.youtube.com/watch?v=HJq-6y2IYEQ',
+    'https://www.youtube.com/watch?v=zrxuPkbIq4g',
+    'https://www.youtube.com/watch?v=iI1M48eC3x4',
+    'https://www.youtube.com/watch?v=EDQ1dmFEGiI',
+    'https://www.youtube.com/watch?v=BPopaJsNWd4',
+]
+YOUTUBE_IDS = YOUTUBE_PLAYLISTS + YOUTUBE_IDS
