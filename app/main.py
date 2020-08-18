f001a9d69ea9bea4c27c4838d1e4ecbd689c5910
df@ <@>
2020-08-18 23:52:30 +0100

    reset original download dir.

--- app/main.py
+++ app/main.py
@@ -7,9 +7,9 @@ import app.data as data
 import app.youtube as youtube
 
 ROOT_DIR = (
-    # os.path.dirname(
+    os.path.dirname(
         os.path.dirname(os.path.abspath(__file__))
-# )
+)
 )
 
 
