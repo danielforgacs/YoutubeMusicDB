455b213cc2818d5eb820d691431e789956a43f4a
df@ <@>
2020-08-20 02:05:23 +0100

    refactor; test update;

--- tests/data_test.py
+++ tests/data_test.py
@@ -7,8 +7,6 @@ import app.youtube as youtube
 
 
 
-
-
 PLAYLIST_DATA = [{
         'id': 'pl-id_{}'.format(idx),
         'title': 'pl-title_{}'.format(idx),
@@ -25,64 +23,20 @@ VIDEO_DATA = [
 
 
 
-
-
 def setup_module():
     tests.setup.init_test_db()
-    # with open(tests.setup.SCHEMA_FILE, 'r') as schemafile:
-    #     schemasql = schemafile.read()
-
-    # with data.PGConnection(dbname='postgres') as conn1:
-    #     conn1.set_isolation_level(
-    #         psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
-    #     cur = conn1.cursor()
-
-    #     try:
-    #         cur.execute(query="drop database %s;" % tests.setup.TEST_DB_NAME)
-    #     except psycopg2.errors.InvalidCatalogName:
-    #         pass
-
-    #     cur.execute(query="create database %s;" % tests.setup.TEST_DB_NAME)
-
-    # with data.PGConnection() as conn2:
-    #     conn2.set_isolation_level(
-    #         psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
-    #     cur = conn2.cursor()
-    #     cur.execute(query=schemasql)
-
-
 
 
 def teardown_module():
     tests.setup.delete_test_db()
-    # with data.PGConnection(dbname='postgres') as conn1:
-    #     conn1.set_isolation_level(
-    #         psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
-    #     cur = conn1.cursor()
-    #     cur.execute(query="drop database %s;" % tests.setup.TEST_DB_NAME)
-
 
 
 def setup():
     tests.setup.delete_test_db_data()
-    # with data.PGConnection() as conn:
-    #     curs = conn.cursor()
-    #     curs.execute("""
-    #         DELETE FROM video;
-    #         DELETE FROM playlist;
-    #     """)
-    #     conn.commit()
 
 
 def teardown():
     tests.setup.delete_test_db_data()
-    # with data.PGConnection() as conn:
-    #     curs = conn.cursor()
-    #     curs.execute("""
-    #         DELETE FROM video;
-    #         DELETE FROM playlist;
-    #     """)
-    #     conn.commit()
 
 
 
