124b59e5f467e09a81b2a65d173c5ff41cca6e09
df@ <@>
2020-08-28 17:19:21 +0100

    wip download & archive

--- app/static/app.js
+++ app/static/app.js
@@ -2,8 +2,10 @@ loadedIndexes           = []
 tableHeaders            = ["pk", "title", "is_down", "added", "youtube_id", "playlist"]
 videoTableID            = "videotable"
 playlistURIinputID      = "playlist_uri"
+archiveBtnID            = "archiveBtn"
 videoTable              = document.getElementById(videoTableID)
 plstInput               = document.getElementById(playlistURIinputID)
+archiveBtn              = document.getElementById(archiveBtnID)
 
 
 window.addEventListener("load", createVideoTable, false)
@@ -34,6 +36,8 @@ function createVideoTable() {
 
 
 function submitPlaylist() {
+    archiveBtn.setAttribute('class', 'btn btn-secondary mb-2 disabled')
+
     plst                = {id: plstInput.value};
     plstInput.value     = "";
 
@@ -79,4 +83,38 @@ function addVidoTableRows(data) {
 
         loadedIndexes.push(video[0])
     }
+}
+
+
+
+function downloadPaylist() {
+    archiveBtn.setAttribute('class', 'btn btn-secondary mb-2 disabled')
+
+    plst                = {id: plstInput.value};
+    plstInput.value     = "";
+
+    let response = fetch('/api/download', {
+        method:     'POST',
+        headers:    {'Content-Type': 'application/json'},
+        body:       JSON.stringify(plst)
+    });
+
+    response
+        .then(response => response.json())
+        .then(createArchiveLink)
+}
+
+
+
+function createArchiveLink(data) {
+    console.log(data)
+    console.log(data.videos)
+    console.log(data.archive)
+    archiveBtn.setAttribute('class', 'btn btn-success mb-2')
+}
+
+
+
+function downloadArchive() {
+    console.log('[ARCHIVE]')
 }
