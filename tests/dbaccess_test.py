2a2fc27148a977118253b2898fbd5da4fa541344
df@ <@>
2020-09-02 22:52:17 +0100

    refactor

--- tests/dbaccess_test.py
+++ tests/dbaccess_test.py
@@ -21,24 +21,24 @@ def test_server_is_working():
 def test_select_all_videos_01():
     dbacces_svr = xmlrpc.client.ServerProxy(uri=tests.setup.DB_ACCESS_URL)
     videos = dbacces_svr.select_all_videos()
-    expected = {}
+    expected = []
 
-    assert videos == {}
+    assert videos == expected
 
 
 
 def test_select_all_videos_02():
     dbacces_svr = xmlrpc.client.ServerProxy(uri=tests.setup.DB_ACCESS_URL)
     videos = dbacces_svr.select_all_videos()
-    expected = {}
+    expected = []
 
-    assert videos == {}
+    assert videos == expected
 
     tests.setup.run_sql_file(sqlfile='testData_03')
 
     videos = dbacces_svr.select_all_videos()
-    expected = {
-        'id1': {
+    expected = [
+        {
             'pk': 1,
             'id': 'id1',
             'title': 'title1',
@@ -47,7 +47,7 @@ def test_select_all_videos_02():
             'is_down': True,
             'playlisttitle': None,
         },
-        'id2': {
+        {
             'pk': 2,
             'id': 'id2',
             'title': 'title2',
@@ -56,7 +56,7 @@ def test_select_all_videos_02():
             'is_down': False,
             'playlisttitle': 'pltitle1',
         },
-        'id3': {
+        {
             'pk': 3,
             'id': 'id3',
             'title': 'title3',
@@ -65,7 +65,7 @@ def test_select_all_videos_02():
             'is_down': False,
             'playlisttitle': None,
         },
-        'id4': {
+        {
             'pk': 4,
             'id': 'id4',
             'title': 'title4',
@@ -74,7 +74,7 @@ def test_select_all_videos_02():
             'is_down': True,
             'playlisttitle': 'pltitle2',
         },
-        'id5': {
+        {
             'pk': 5,
             'id': 'id5',
             'title': 'title5',
@@ -83,6 +83,6 @@ def test_select_all_videos_02():
             'is_down': False,
             'playlisttitle': 'pltitle2',
         },
-    }
+    ]
 
     assert videos == expected
