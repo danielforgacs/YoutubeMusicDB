17e1a0db64b83e9316ca97807c048202fa835d67
df@ <@>
2020-09-04 11:25:33 +0100

    clean up

--- sql/video_by_playlist_id.sql
+++
@@ -1,9 +0,0 @@
-\set plistid '\'PL9YsudagsL6hicXrha4zBId875lRXxc32\''
-
-select * from video;
-
-SELECT video.id
-FROM video
-JOIN playlist ON playlist.pk = video.playlist
-WHERE playlist.id = :plistid
-;
