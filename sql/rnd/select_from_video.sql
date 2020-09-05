96f8262d7121cfbb9d1da622ba4f1026fc66cac7
df@ <@>
2020-09-05 01:55:58 +0100

    many to many playlist to video select rnd works

--- sql/rnd/select_from_video.sql
+++ sql/rnd/select_from_video.sql
@@ -1,6 +1,14 @@
 select * from video order by pk;
 select * from playlist_video order by video_pk;
 
+select array [playlist.id, playlist.title]
+from playlist
+;
+
+select array_agg( array [playlist.id, playlist.title])
+from playlist
+;
+
 select
     video.pk,
     video.id,
@@ -12,9 +20,13 @@ select
         select playlist_video.playlist_pk
         from playlist_video
         where playlist_video.video_pk = video.pk
-    ) as playlist_pks
+    ) as playlist_pks,
+    (
+        select array_agg ( array [ playlist.id, playlist.title ])
+        from playlist
+        join playlist_video on playlist_video.playlist_pk = playlist.pk
+        where video.pk = playlist_video.video_pk
+    ) as playlist_data
 from video
-left join playlist_video on playlist_video.video_pk = video.pk
-group by pk
-order by pk
+order by video.pk
 ;
